5755c5c0a109705a961ae392b6426127bcb1b17c
Nick Castle <nick@uprighted.com>
2022-10-04 10:16:32 -0400

    configure webpack, babel, react, and css loaders

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './styles/styles.css';
+
+const root = ReactDOM.createRoot(document.getElementById('dynamic'));
+
+root.render(<h3>Hello from React</h3>);
