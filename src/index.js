fb55443bfc50e472e24959d4888897119abcaa7b
Nick Castle <nick@uprighted.com>
2022-10-04 16:39:30 -0400

    create an App component to contain application jsx

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import './styles/styles.css';
+import App from './app';
 
-const root = ReactDOM.createRoot(document.getElementById('dynamic'));
-
-root.render(<h3>Hello from React</h3>);
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
