8b692b43105a38409ab77735972500e1884833a6
Nick Castle <nick@uprighted.com>
2022-10-07 11:50:44 -0400

    add basic add and delete functionality

--- src/app.js
+++ src/app.js
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  return <h1>Hello from my App component</h1>;
+  const [items, setItems] = useState([]);
+  const [exercise, setExercise] = useState('');
+
+  const handleChange = (evt) => {
+    const { value } = evt.target;
+    setExercise(value);
+  };
+
+  const handleSubmit = () => {
+    if (!exercise) return;
+    const itemsCopy = [...items];
+    itemsCopy.push(exercise);
+    setItems(itemsCopy);
+    setExercise('');
+  };
+
+  const handleDelete = (i) => {
+    console.log('im deleting index ', i);
+    const itemsCopy = [...items];
+    itemsCopy.splice(i, 1);
+    setItems(itemsCopy);
+  };
+
+  console.log(exercise);
+
+  return (
+    <>
+      <h1>Workout App</h1>
+      <form
+        onSubmit={(evt) => {
+          evt.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <input
+          className='br-025'
+          name='exercise'
+          placeholder='Exercise'
+          value={exercise}
+          onChange={handleChange}
+        />
+        <button id='submit' className='br-025' onClick={handleSubmit}>
+          Log Exercise
+        </button>
+      </form>
+
+      {!!items.length && (
+        <ul>
+          {items.map((item, i) => (
+            <li className='item br-025' key={i}>
+              {item}
+              <button className='br-025' onClick={() => handleDelete(i)}>
+                delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default App;
